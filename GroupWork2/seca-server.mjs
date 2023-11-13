988c2be39d072306ea1dd7ed6b422e9b91ff25fa
ilianosantos <ilianosantos8@gmail.com>
2023-11-13 01:38:58 +0000

    Update on seca-server and add json files

--- GroupWork2/seca-server.mjs
+++ GroupWork2/seca-server.mjs
@@ -1,5 +1,22 @@
 import './seca-web-api.mjs'
-import {supertest} from 'supertest';
-import {express} from 'express';
+import express from 'express';
 var app = express();
 
+const PORT = 2003;
+console.log(" Starting server on port", PORT);
+app.get('/', (req, res) => {
+  res.send('Welcome to the SECA API');
+});
+
+app.get('/events/list/:s/:p', getPopularEvents);
+app.get('/events/search/:s/:p', getEventNames);
+app.post('/group/create', createGroups);
+app.put('/group/edit', editGroups);
+app.get('/group/list', listGroups);
+app.delete('/group/delete', deleteGroups);
+app.put('/group/add_events', addEvents);
+app.delete('/group/remove_events', removeEvents);
+app.post('/users', users);
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
--- GroupWork2/tm-events-data.mjs
+++ GroupWork2/tm-events-data.mjs
@@ -1,6 +1,7 @@
 import './seca-data-mem.mjs'
-import { fetch } from 'node-fetch';
+import fetch from 'node-fetch';
 
-const URL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=SRgOVLFAXnlAaniGoODNS3iGzIXyOqI6`;
+const KEY = 'SRgOVLFAXnlAaniGoODNS3iGzIXyOqI6';
+const URL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${KEY}`;
 
 fetch(URL)
