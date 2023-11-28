79d070eecd41e599f9efcbaf7d2264537ad38b25
danielffcarvalho <a49503@alunos.isel.pt>
2023-11-28 15:15:46 +0000

    Writes the events on output.json

--- GroupWork2/tm-events-data.mjs
+++ GroupWork2/tm-events-data.mjs
@@ -1,12 +1,15 @@
-import './seca-data-mem.mjs'
+import './seca-data-mem.mjs';
 import fetch from 'node-fetch';
-import errors from './errors.mjs'
+import errors from './errors.mjs';
+import fs from 'fs';
+const apiKey = 'YfmQlHOLFgYdtMQaFbAK7noAe283pmaV';
+const outputFile = 'output.json';
 
 export async function getAllPopularEventsList(limit) {
     const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events/?sort=relevance,desc&apikey=${apiKey}`)
     const events = await data.json()
-    if (eventData._embedded && eventData._embedded.events){
-        return events.items.slice(0, limit)
+    if (events._embedded && events._embedded.events){
+        return events._embedded.events.slice(0, limit)
     }
         throw errors.INVALID()
 }
@@ -20,3 +23,55 @@ export async function getEventsByName(name, limit) {
     }
       throw errors.INVALID()
 }
+
+async function fetchEventDetails(events) {
+  try {
+    return await Promise.all(events);
+  } catch (error) {
+    console.error('Error fetching event details:', error);
+    throw error;
+  }
+}
+
+function formatEventDetails(eventDetails) {
+  return eventDetails.map(function (event) {
+    return {
+      id: event.id,
+      name: event.name,
+      date: event.dates.start.dateTime,
+      segment: event.classifications[0].segment.name,
+      genre: event.classifications[0].genre.name,
+    };
+  });
+}
+
+async function writeFile(formattedEventDetails) {
+  return new Promise((resolve, reject) => {
+    const outputData = {
+      events: formattedEventDetails,
+    };
+
+    fs.writeFile(outputFile, JSON.stringify(outputData, null, 2), (err) => {
+      if (err) {
+        console.error('Error writing to output file:', err);
+        reject(err);
+      } else {
+        console.log('Event details written to Async-await-event-details.json');
+        resolve();
+      }
+    });
+  });
+}
+
+async function main() {
+  try {
+    const events = await getEventsByName("Phoenix Suns vs. Memphis Grizzlies");
+    const eventDetails = await fetchEventDetails(events);
+    const formattedEventDetails = formatEventDetails(eventDetails);
+    await writeFile(formattedEventDetails);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+main()
